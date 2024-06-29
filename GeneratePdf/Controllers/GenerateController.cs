4ae8549aef316751e04c538f7401bc4996756ff0
Abdelrahman-Hussen <abdelrahmanhussein000@gmail.com>
2024-06-29 16:30:47 +0300

    Add project files.

---
+++ GeneratePdf/Controllers/GenerateController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Templating.Core;
+
+namespace GeneratePdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateController : ControllerBase
+    {
+        private readonly ILogger<GenerateController> _logger;
+        public GenerateController(ILogger<GenerateController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GeneratePdf()
+        {
+            var invoiceFactory = new InvoiceFactory();
+
+            var invoice = invoiceFactory.Create();
+
+            var html = await RazorTemplateEngine.RenderAsync("Temps/Invoice.cshtml", invoice);
+
+            var rendered = new ChromePdfRenderer();
+
+            using var pdf = rendered.RenderHtmlAsPdf(html);
+
+            pdf.SaveAs($"wwwroot/invoices/test{invoice.Number}.pdf");
+             
+            return File(pdf.BinaryData, "application/pdf", $"test-{invoice.Number}.pdf");
+        }
+    }
+}
---
+++ GeneratePdf/InvoiceFactory.cs
@@ -0,0 +1,73 @@
+﻿using Bogus;
+
+namespace GeneratePdf
+{
+    public class InvoiceFactory
+    {
+        public Invoice Create()
+        {
+            var faker = new Faker();
+
+            var invoice = new Invoice
+            {
+                Number = faker.Random.Number(100_000, 1_000_000).ToString(),
+                IssuedDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                DueDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10)),
+                SellerAddress = new Address
+                {
+                    CompanyName = faker.Company.CompanyName(),
+                    Street = faker.Address.StreetName(),
+                    City = faker.Address.City(),
+                    State = faker.Address.State(),
+                    Email = faker.Internet.Email(),
+                },
+                CustomerAddress = new Address
+                {
+                    CompanyName = faker.Company.CompanyName(),
+                    Street = faker.Address.StreetName(),
+                    City = faker.Address.City(),
+                    State = faker.Address.State(),
+                    Email = faker.Internet.Email(),
+                },
+                LineItems = Enumerable
+                    .Range(1, 10)
+                    .Select(i => new LineItem
+                    {
+                        Id = i,
+                        Name = faker.Commerce.ProductName(),
+                        Price = faker.Random.Decimal(10, 1000),
+                        Quantity = faker.Random.Decimal(1, 10)
+                    }).ToList(),
+            };
+
+            return invoice;
+        }
+    }
+
+    public class Address
+    {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public object Email { get; set; }
+    }
+
+    public sealed class Invoice
+    {
+        public string Number { get; set; } = string.Empty;
+        public DateOnly IssuedDate { get; set; }
+        public DateOnly DueDate { get; set; }
+        public Address SellerAddress { get; set; }
+        public Address CustomerAddress { get; set; }
+        public List<LineItem> LineItems { get; set; } = [];
+    }
+
+    public sealed class LineItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
